451aab36818741853906fe07a04c0a318ceb552b
Rossfurniture <depasqualeross@gmail.com>
2026-07-29 20:54:46 +0800

    Update page.tsx

--- app/thank-you/page.tsx
+++ app/thank-you/page.tsx
@@ -1,5 +1,24 @@
+import Script from "next/script";
 import ThankYouPage from "./ThankYouPage";
 
 export default function Page() {
-  return <ThankYouPage />;
+  return (
+    <>
+      {/* Google Ads lead conversion */}
+      <Script
+        id="google-ads-lead-conversion"
+        strategy="afterInteractive"
+      >
+        {`
+          gtag("event", "conversion", {
+            send_to: "AW-18356986395/0699CLq-j9gcEJvEpbFE"
+          });
+
+          gtag("event", "generate_lead");
+        `}
+      </Script>
+
+      <ThankYouPage />
+    </>
+  );
 }
